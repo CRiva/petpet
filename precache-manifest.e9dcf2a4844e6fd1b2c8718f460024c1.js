2c6c8f13b86fa61397f7c5d0cc2b3caff7a4885d
Connor Riva <CRiva94@gmail.com>
2020-07-24 09:58:38 -0700

    Updates

---
+++ precache-manifest.e9dcf2a4844e6fd1b2c8718f460024c1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f31f35d042e2623c7174dd010b59c6",
+    "url": "/petpet/index.html"
+  },
+  {
+    "revision": "756bb7ee951c21a6b941",
+    "url": "/petpet/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "daed70bc15dbc91f2f11",
+    "url": "/petpet/static/css/main.4d218b22.chunk.css"
+  },
+  {
+    "revision": "756bb7ee951c21a6b941",
+    "url": "/petpet/static/js/2.468f0014.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/petpet/static/js/2.468f0014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daed70bc15dbc91f2f11",
+    "url": "/petpet/static/js/main.8973c15f.chunk.js"
+  },
+  {
+    "revision": "62e63ab548189ace5a1c",
+    "url": "/petpet/static/js/runtime-main.d609a8d4.js"
+  },
+  {
+    "revision": "b2041793b2b5a1631c260653024b9846",
+    "url": "/petpet/static/media/bathSound.b2041793.mp3"
+  },
+  {
+    "revision": "e036d31ddc48ef929622752a64d23e84",
+    "url": "/petpet/static/media/eatSound.e036d31d.mp3"
+  },
+  {
+    "revision": "ca70f5f6efff8b546c904be9394c5af3",
+    "url": "/petpet/static/media/foodBowl.ca70f5f6.png"
+  },
+  {
+    "revision": "55db9858cd22d02846e5598a4f597127",
+    "url": "/petpet/static/media/happy.55db9858.png"
+  },
+  {
+    "revision": "1f8bb20022e213cd4264a4d0d5bf8782",
+    "url": "/petpet/static/media/lol.1f8bb200.mp3"
+  },
+  {
+    "revision": "bee15c3abf949e22cd0b4a6421af3bf8",
+    "url": "/petpet/static/media/netural.bee15c3a.png"
+  },
+  {
+    "revision": "4c0696ef60ff46962ca30e7998a50283",
+    "url": "/petpet/static/media/pickPoop.4c0696ef.mp3"
+  },
+  {
+    "revision": "d9686baca22bd87521ff3ff63a6d785f",
+    "url": "/petpet/static/media/poop.d9686bac.png"
+  },
+  {
+    "revision": "eec99f7bcf57c8d581cb5b5514dce131",
+    "url": "/petpet/static/media/sad.eec99f7b.png"
+  },
+  {
+    "revision": "064675d75c887bfa8d417ae2cccfd9d9",
+    "url": "/petpet/static/media/washtub.064675d7.png"
+  }
+]);
