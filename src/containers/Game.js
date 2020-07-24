3bc6a583d95f02669d1ac4630cfbef0d9ea31bb3
Chris Yang <haocheng.yang@appfolio.com>
2020-07-23 17:05:32 -0700

    cy/cr - feeding pet will also increase happiness, if there are 6 poops on the screen, the pet will die
    Author:    Chris Yang <haocheng.yang@appfolio.com>
    Co-authored-by: Connor Riva <CRiva94@gmail.com>

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -8,11 +8,10 @@ import ActionBar from '../components/ActionBar';
 import { useInterval } from '../helpers/useInterval';
 import PoopArea from '../components/PoopArea';
 import poop from '../poop.png';
-
+import HiddenArea from '../components/HiddenArea';
 
 
 const Game = ({ gameStore }) => {
-
   useEffect(() => {
     const height = document.getElementById('poop-area').clientHeight;
     const width = document.getElementById('poop-area').clientWidth;
@@ -26,8 +25,9 @@ const Game = ({ gameStore }) => {
   };
   const cleanPet = () => {
     while (gameStore.poop.poops>0) {
-      gameStore.poop.poops -=1;
+
       gameStore.poop.poopArea.removeChild(gameStore.poop.poopArea.lastChild);
+      gameStore.poop.poops -=1;
   }
     gameStore.dirtiness.percentage = 0;
     if (gameStore.isHungry){
@@ -41,25 +41,56 @@ const Game = ({ gameStore }) => {
     gameStore.updateAttribute(gameStore.happiness, 5);
   };
 
+
   useInterval(() => {
     gameStore.updateAttributes();
   }, 1000);
 
 
   useInterval(() => {
-    gameStore.poopStore.createPoop();
-  }, 5000);
+    gameStore.poop.createPoop();
+  }, 3000);
 
-  // const resetInterval = () => setCount(0);
 
-  // if (gameStore.dirtiness == 100){
-  //   resetInterval();
-  // }
+   const clearAll = (()=> {
+    for (var i = setTimeout(function() {}, 0); i > 0; i--) {
+      window.clearInterval(i);
+      window.clearTimeout(i);
+      if (window.cancelAnimationFrame) window.cancelAnimationFrame(i);
+    }
+  });
+
+  useInterval(() => {
+    
+    if(gameStore.poop.poops > 5){
+      while (gameStore.poop.poopArea.hasChildNodes()) {
+        gameStore.poop.poopArea.removeChild(gameStore.poop.poopArea.lastChild);
+      }
+      clearAll();
+      const end = document.querySelector('.gameOver').cloneNode(true);
+      const restart = document.getElementById('hiddenPoop').cloneNode(true);
+      restart.style.display = 'block';
+      restart.onclick = () =>{
+        window.location.reload(); 
+      };
+      end.style.display = 'block';
+      gameStore.poop.poopArea.appendChild(end);
+      gameStore.poop.poopArea.appendChild(restart);
+    }
+  }, 6000);
+
+  
 
   return useObserver(() => (
     <div>
+      <HiddenArea>
+        <img src={poop} className="poop" id ="hiddenPoop" alt="Poop"/>
+        <div className="gameOver">
+          <h1>Your pet died from being burried in it's own crap... Are you sure you're ready to be a pet owner?</h1>
+          <h1>Click the poop to restart</h1>
+        </div>
+      </HiddenArea>
       <PoopArea>
-        <img src={poop} className="poop" id ="hiddenPoop" alt="Poop" />
         <StatusHeader gameStore={gameStore} />
         <br />
         <br />
